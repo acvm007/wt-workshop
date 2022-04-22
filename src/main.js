8d70f4d8dad63cc1590d8cc1333c16af9086ee1e
Denise Büning <denise24111993@gmail.com>
2022-04-22 20:27:48 +0200

    Update Styles

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "https://webmachinelearning.github.io/webnn-polyfill/dist/webnn-polyfill.js"
 
+import "./style/style.scss"
+
 createApp(App).mount('#app')
