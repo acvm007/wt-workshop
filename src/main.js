d6f97a16302388303be8c72ad6da30a9ac9a056b
Aljosha Merz <am@pay4lunch.de>
2023-04-10 10:19:16 +0200

    Init

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import "https://webmachinelearning.github.io/webnn-polyfill/dist/webnn-polyfill.js"
-
-import "./style/style.scss"
-
-createApp(App).mount('#app')
